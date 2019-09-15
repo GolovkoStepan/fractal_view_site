a16e46f5a03ce05df93f6590f0361398ab171ed4
SSmitt <lalaproger@yandex.ru>
2019-09-15 15:05:26 +0200

    new links and rename "download"

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: "site#index"
 
-  get "about", to: "site#about"
+  get "download", to: "site#download"
   get "author", to: "site#author"
   get "users_works", to: "site#users_works"
 end
