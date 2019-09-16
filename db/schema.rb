471ca7363ff22799428a4b9f7c1be1e90b3f7b49
GolovkoStepan <stepagolovko1993@gmail.com>
2019-09-16 22:16:37 +0500

    create styles for nav

--- db/schema.rb
+++ db/schema.rb
@@ -12,12 +12,34 @@
 
 ActiveRecord::Schema.define(version: 2019_09_15_163333) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "social_media", force: :cascade do |t|
     t.string "site_name"
     t.string "site_url"
     t.string "icon_class"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "age"
+    t.string "gender"
+    t.text "bio"
+    t.integer "status"
+    t.integer "role"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
-SocialMedia.create!(site_name: "ВКонтакте", site_url: "https://vk.com/id45986033", icon_class: "fab fa-vk" )
-SocialMedia.create!(site_name: "Телеграм", site_url: "https://telegram/@stepangolovko", icon_class: "fab fa-telegram" )
-SocialMedia.create!(site_name: "GitHub", site_url: "https://github.com/GolovkoStepan", icon_class: "fab fa-github" )
+#SocialMedia.create!(site_name: "ВКонтакте", site_url: "https://vk.com/id45986033", icon_class: "fab fa-vk" )
+#SocialMedia.create!(site_name: "Телеграм", site_url: "https://telegram/@stepangolovko", icon_class: "fab fa-telegram" )
+#SocialMedia.create!(site_name: "GitHub", site_url: "https://github.com/GolovkoStepan", icon_class: "fab fa-github" )
 
-User.create!(email: "admin@admin.com", password: "123123123", password_confirmation: "123123123")
+#User.create!(email: "admin@admin.com", password: "123123123", password_confirmation: "123123123")
