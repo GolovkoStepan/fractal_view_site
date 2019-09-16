c29470d86937018471c53aa776ae6ddfcf8cf943
SSmitt <lalaproger@yandex.ru>
2019-09-16 14:35:01 +0200

    links social and logo

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+SocialMedia.create!(site_name: "ВКонтакте", site_url: "https://vk.com/id45986033", icon_class: "fab fa-vk" )
+
+SocialMedia.create!(site_name: "Телеграм", site_url: "https://telegram/@stepangolovko", icon_class: "fab fa-telegram" )
+
+SocialMedia.create!(site_name: "GitHub", site_url: "https://github.com/GolovkoStepan", icon_class: "fab fa-github" )
