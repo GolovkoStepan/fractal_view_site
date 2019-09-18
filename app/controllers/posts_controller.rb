d452c87acc9e18d8a06131e23ae2e7a9e7895934
GolovkoStepan <stepagolovko1993@gmail.com>
2019-09-18 23:06:28 +0500

    change post img link position

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
 
-  skip_before_action :authenticate_user!, only: :index
+  skip_before_action :authenticate_user!, only: [ :index, :show ]
 
   def index
     @posts = Post.order(:title).page params[:page]
