f724de091c435558b1e814df2db86cea299e8636
GolovkoStepan <stepagolovko1993@gmail.com>
2019-09-27 11:13:09 +0500

    little fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,12 +4,6 @@ SocialMedia.create!(site_name: "GitHub", site_url: "https://github.com/GolovkoSt
 
 User.create!(email: "admin@admin.com", password: "123123123", password_confirmation: "123123123", role: "admin")
 
-#p = Post.new
-#p.title = "Новое изображение фрактала"
-#p.description = "Координаты: 0.843643255732, 0.2374853563475"
-#p.main_picture.attach(io: File.open("#{Rails.root}/app/assets/images/fractal_1.png"), filename: 'fractal_1.png')
-#p.user = User.find(2)
-#p.save!
 
 #ActiveRecord::Base.connection.tables.each do |table_name|
 #  ActiveRecord::Base.connection.reset_pk_sequence!(table_name)
